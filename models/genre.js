bf457b2b2274cf2b11834ed0deb3f3d9a0b16094
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2026-07-29 14:31:30 +0700

    feat: add Genre model definition

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+module.exports = (sequelize, DataTypes) => {
+    const Genre = sequelize.define("Genre", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        genre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    });
+    return Genre;
+};
